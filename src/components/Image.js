bacda6717b9e814680b3d920ba6bad51509a80c9
Wim von Benecke <boneswvb@gmail.com>
2020-03-20 08:12:22 +0200

    final upload.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,17 +1,51 @@
-import React, {useState} from "react"
+import React, {useContext} from "react"
+import PropTypes from "prop-types"
+
+import {Context} from '../Context'
+import useHover from "../hooks/useHover"
 
 function Image({className, img}) {
-	const [hovered, setHovered] = useState(false)
-	
+	const [hovered, ref] = useHover()
+  const {toggleFavorite, addToCart, cartItems, removeFromCart} = useContext(Context)
+
+	const heartIcon = () => {
+    if(img.isFavorite) {
+      return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+    } else if (hovered) {
+      return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+    }
+  } 
+
+	const cartIcon = () => {
+    const alreadyInCart = cartItems.some(item => item.id === img.id)
+      if(alreadyInCart) {
+        return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img.id)}></i>       
+      } else if (hovered) {
+        return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+      }
+    } 
+
   return (
-    <div className={`${className} image-container`}>
-      <img 
-      	onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)} 
-      	src={img.url} 
-      	alt="Images for sale" className="image-grid"/>
+    <div 
+    	className={`${className} image-container`}
+       ref={ref}
+      >
+      
+      <img src={img.url} alt="Images for sale" className="image-grid"/>
+    	{heartIcon()}
+    	{cartIcon()}
     </div>
   )
 }
 
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
+}
+
 export default Image
+
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,39 @@
-import React from "react"
+import React, {useState, useContext} from "react"
+import {Context} from "../Context"
+import CartItem from "../components/CartItem"
 
 const Cart = () => {
-  return (
-    <main className="cart-page">
-      	<h1>Check out</h1>
-    </main>
-  )
+    const [buttonText, setButtonText] = useState("Place order")
+    const {cartItems, emptyCart} = useContext(Context)
+    const totalCost = 5.99 * cartItems.length
+    const totalCostDisplay = totalCost.toLocaleString("en-ZA", {style: "currency", currency: "ZAR"})
+    
+    const cartItemElements = cartItems.map(item => (
+      <CartItem key={item.id} item={item} />
+    ))
+
+    const placeOrder = () => {
+      setTimeout(() => {
+       console.log("Order placed!")
+        setButtonText("Place Order")
+        emptyCart()
+      }, 3000)
+    }
+    
+    return (
+      <main className="cart-page">
+        <h1>Check out</h1>
+        {cartItemElements}
+        <p className="total-cost">Total: {totalCostDisplay}</p>
+        {
+          cartItems.length > 0 ?
+          <div className="order-button">
+            <button onClick={placeOrder}>{buttonText}</button>
+          </div> :
+          <p>You have no items in your cart.</p>
+            }
+      </main>
+    )
 }
 
 export default Cart
