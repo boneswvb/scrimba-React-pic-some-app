3fd305527a92ef7d981903bc2318593937b63539
Wim von Benecke <boneswvb@gmail.com>
2020-04-22 09:43:35 +0200

    Updates

---
+++ precache-manifest.ae9162b87014709559635869eacbc29d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b95a17f9bb3a4d678780ead4c64c3b",
+    "url": "/scrimba-react-pic-some-app/index.html"
+  },
+  {
+    "revision": "5a28bcb554972b4d0a0a",
+    "url": "/scrimba-react-pic-some-app/static/css/main.40d4f8c5.chunk.css"
+  },
+  {
+    "revision": "ba667d87cec8a3d1014a",
+    "url": "/scrimba-react-pic-some-app/static/js/2.91d1e597.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/scrimba-react-pic-some-app/static/js/2.91d1e597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a28bcb554972b4d0a0a",
+    "url": "/scrimba-react-pic-some-app/static/js/main.0b95e31e.chunk.js"
+  },
+  {
+    "revision": "c6c9f2e2d8b32802ec77",
+    "url": "/scrimba-react-pic-some-app/static/js/runtime-main.d9e90999.js"
+  }
+]);
